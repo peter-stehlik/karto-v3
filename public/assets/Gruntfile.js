bc8973073508e111cc8883aff3f311575f8bca8e
Peter Stehlik <peter@inovative.sk>
2021-01-29 17:39:44 +0100

    prepare Bootstrap Sass

---
+++ public/assets/Gruntfile.js
@@ -0,0 +1,118 @@
+module.exports = function (grunt) {
+	// Force use of Unix newlines
+	grunt.util.linefeed = "\n";
+  
+	grunt.initConfig({
+	  pkg: grunt.file.readJSON("package.json"),
+  
+	  sass: {
+		dist: {
+		  options: {
+			style: "expanded"
+		  },
+		  files: {
+			"dist/css/bootstrap.5.0.0.css": "dev/scss/bootstrap/bootstrap.scss",
+		  }
+		}
+	  },
+  
+	  postcss: {
+		options: {
+		  map: false, // inline sourcemaps
+  
+		  processors: [
+			require("autoprefixer")(), // add vendor prefixes
+			require("cssnano")() // minify the result
+		  ]
+		},
+		dist: {
+		  src: "dist/css/custom.css",
+		  dest: 'dist/css/custom.min.css'
+		}
+	  },
+  
+	  imagemin: {
+		// Task
+		dynamic: {
+		  // Another target
+		  files: [
+			{
+			  expand: true, // Enable dynamic expansion
+			  cwd: "images/raw/", // Src matches are relative to this path
+			  src: ["*.{png,jpg,gif}"], // Actual patterns to match
+			  dest: "images/" // Destination path prefix
+			}
+		  ]
+		}
+	  },
+  
+	  watch: {
+		scripts: {
+		  files: ["dev/scss/**/*.scss", "dev/es6/**/*.js"],
+		  tasks: ["sass", "babel", "eslint:build"],
+		  options: {
+			interrupt: true,
+			spawn: false
+		  }
+		}
+	  },
+  
+	  concat: {
+		dev: {
+		  src: [
+			'dist/js/bootstrap.bundle.min.js',
+			'dist/js/custom.js'
+		  ],
+		  dest: 'dist/js/main.js'
+		}
+	  },
+  
+	  eslint: {
+		build: {
+		  options: {
+			configFile: 'dev/es6/eslint.json'
+		  },
+		  src: ['dist/js/custom.js']
+		},
+	  },
+  
+	  babel: {
+		options: {
+		  sourceMap: false,
+		  presets: ['@babel/preset-env']
+		},
+		dist: {
+		  files: {
+			"dist/js/custom.js": "dev/es6/custom.js",
+		  }
+		}
+	  },
+  
+	  uglify: {
+		main: {
+		  files: {
+			"dist/js/custom.min.js": "dist/js/custom.js"
+		  }
+		}
+	  },
+  
+	  htmllint: {
+		all: ["*.html"]
+	  }
+	  
+	});
+  
+	grunt.registerTask("dev", ["sass", "babel", "eslint:build", "htmllint"]);
+	grunt.registerTask("build", ["sass", "babel", "eslint:build", "postcss", "imagemin", "htmllint", "uglify"]);
+  
+	grunt.loadNpmTasks("grunt-contrib-sass");
+	grunt.loadNpmTasks("grunt-postcss");
+	grunt.loadNpmTasks("grunt-contrib-imagemin");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-babel");
+	grunt.loadNpmTasks("grunt-eslint");
+	grunt.loadNpmTasks("grunt-html");
+  };
+  
