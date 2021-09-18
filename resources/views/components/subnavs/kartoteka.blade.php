816e2e02c994dd180ab9810e335ccf480ea89a84
Peter Stehlik <peter@inovative.sk>
2021-09-18 06:31:22 +0200

    enable active class for Kartoteka subpages

--- resources/views/components/subnavs/kartoteka.blade.php
+++ resources/views/components/subnavs/kartoteka.blade.php
@@ -1,18 +1,18 @@
 <ul class="nav flex-column">
 	<li class="nav-item">
-		<a class="nav-link" href="{{ route('kartoteka.prijem-get') }}">
+		<a class="nav-link @if( request()->route()->getName() === 'kartoteka.prijem-get' ) active @endif" href="{{ route('kartoteka.prijem-get') }}">
 			Príjem platby
 		</a>
 	</li><!-- / .nav-item -->
 
 	<li class="nav-item">
-		<a class="nav-link" href="{{ route('kartoteka.nepotvrdene-prijmy.index') }}">
+		<a class="nav-link @if( request()->route()->getName() === 'kartoteka.nepotvrdene-prijmy.index' ) active @endif" href="{{ route('kartoteka.nepotvrdene-prijmy.index') }}">
 			Zoznam príjmov
 		</a>
 	</li><!-- / .nav-item -->
 
 	<li class="nav-item">
-		<a class="nav-link" href="{{ route('kartoteka.nepotvrdene-prevody-get') }}">
+		<a class="nav-link @if( request()->route()->getName() === 'kartoteka.nepotvrdene-prevody-get' ) active @endif" href="{{ route('kartoteka.nepotvrdene-prevody-get') }}">
 			Zoznam prevodov
 		</a>
 	</li><!-- / .nav-item -->
@@ -24,7 +24,7 @@
 	</li><!-- / .nav-item -->
 
 	<li class="nav-item">
-		<a class="nav-link" href="{{ route('kartoteka.unconfirmed-incomes-summary-get') }}">
+		<a class="nav-link @if( request()->route()->getName() === 'kartoteka.unconfirmed-incomes-summary-get' ) active @endif" href="{{ route('kartoteka.unconfirmed-incomes-summary-get') }}">
 			Zaúčtovať
 		</a>
 	</li><!-- / .nav-item -->
