62ab87eb386c9cef2f22edbca0206d3310338a2d
Peter Stehlik <peter@inovative.sk>
2021-09-20 13:31:14 +0200

    install PDF generator package

--- config/app.php
+++ config/app.php
@@ -162,9 +162,13 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
+        Barryvdh\DomPDF\ServiceProvider::class,
+
+
 
         /*
          * Application Service Providers...
@@ -226,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
