87a265303743c851411fc414baf0a5dd9559cb46
Peter Stehlik <peter@inovative.sk>
2021-12-07 19:33:25 +0100

    play around with printing CSS

--- resources/views/v-tlac/person.blade.php
+++ resources/views/v-tlac/person.blade.php
@@ -3,27 +3,33 @@
 	<style>
 		html, body {
 			margin: 0;
+			font-family: monospace;
+			font-weight: 300;
+			letter-spacing: 0.2px;
 		}
 		body {
-			font-size: 14px;
-			color: #333;
+			font-size: 16px;
 		}
 		p {
-			margin: 0;
+			margin: 0 0 5px;
 		}
 		.sided {
+			
 			display: flex;
 			justify-content: space-between;
+		}
+		.mw {
+			width: 320px;
 		}
 	</style>
 </head>
 <body>
-	<p class="sided">
+	<p class="mw sided">
 		<span>{!! $person->title !!}</span>
-		<span>{!! $person->id !!}</span>
+		<span class="padding-left: 20px;">{!! $person->id !!}</span>
 	</p>
 
-	<p><span>{!! $person->name1 !!}</span></p>
+	<p class="mw"><span>{!! $person->name1 !!}</span></p>
 	
 	<p>{!! $person->address1 !!}</p>
 
