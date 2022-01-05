14524423bf9bcc77cf5e2dd4978417cbd589e436
Peter Stehlik <peter@inovative.sk>
2022-01-05 08:44:29 +0100

    print on old printers

--- resources/views/v-tlac/new/people-lx-350.blade.php
+++ resources/views/v-tlac/new/people-lx-350.blade.php
@@ -1,5 +1,10 @@
 <html>
 <head>
+	<!-- WORKING FOR EPSON LX 350
+	 
+		Control Panel - Hardware and Software - Devices and Printers [Print server properties]
+		v nastaveniach papiera rozmer 10 x 3.8 cm
+	-->
 	<style>
 		html, body {
 			margin: 0;
